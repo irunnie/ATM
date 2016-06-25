6d3e015801f2a529160d3982ff280a7669298da4
irunnie <irunnie@gmail.com>
2016-06-25 05:15:50 +0300

    commit

---
+++ src/main/java/com/kohanevich/controller/DepositController.java
@@ -0,0 +1,22 @@
+package com.kohanevich.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Closed on 25.06.2016.
+ */
+
+@WebServlet("/deposit")
+public class DepositController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("pages/deposit.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
